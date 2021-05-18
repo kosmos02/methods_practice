4885080ce37be435dd1dd5ec1b16a2296d5184ff
kosmos02 <alexandrgabe@gmail.com>
2021-05-17 20:41:57 -0600

    added other method questions

--- methods_practice.js
+++ methods_practice.js
@@ -60,3 +60,31 @@ const charactersByEyeColor = characters.reduce((acc, cur) => {
 }, {})
 console.log(charactersByEyeColor)
 
+
+// MAP
+// Get an array of all names
+// Get an array of all heights
+// Get an array of objects with just name and height properties
+// Get an array of all first names
+
+// FILTER
+// Get characters with mass greater than 100
+// Get characters with height less than 200
+// Get all male characters
+// Get all female characters
+// SORT
+// Sort by name
+// Sort by mass
+// Sort by height
+// Sort by gender
+// EVERY
+// Does every character have blue eyes?
+// Does every character have mass more than 40?
+// Is every character shorter than 200?
+// Is every character male?
+// SOME
+// Is there at least one male character?
+// Is there at least one character with blue eyes?
+// Is there at least one character taller than 200?
+// Is there at least one character that has mass less than 50?
+
